a1e4964041adad2798e8cedb9c1d125ff5ff2cf8
Daniel Jurczak <daniel.jurczak@imba.oeaw.ac.at>
2014-05-23 10:58:10 +0200

    Fixed crash while saving to disk
    Calling #shutdown only presists data to the disk if the redis config
    contains at least one SAVEPOINT.
    
    When we were shutting the redis server down redis-rb raised a
    TimeoutException because the saving was taking too much time. We wrapped
    the call to shutdown around a begin/rescue block where we loop until we
    see confirmation that the Redis process is infact shutdown.

--- lib/sync_to_redis.rb
+++ lib/sync_to_redis.rb
@@ -1,35 +1,52 @@
-require 'shell_command'
+require 'command'
 require 'redis'
 
 class SyncToRedis
   def convert_to_frequency_files(path_to_bed)
     path_to_uniq_file = path_to_bed.gsub(".split", ".uniq")
     cmdline = "cat #{path_to_bed} | ruby -nae 'puts $F[3]' | uniq -c > #{path_to_uniq_file}"
-    cmd = ShellCommand.run(cmdline)
+    cmd = Command.run(cmdline)
 
     return path_to_uniq_file
   end
 
-  def push_frequencies_to_redis(redis_connection, file_input)
+  ##      38 chr2l_20747989_20748020
+  def push_frequencies_to_redis(redis_connection, file_input, opts)
     file_input.each_with_index do |line, i|
-      line = line.strip
-      fields = line.split(" ")
-      title = fields[1].split("_")[0..-2].join("_")
-      count = fields[0].to_i
+      data = line_to_data(line, opts)
 
-      redis_connection.incrby(title, count)
+      redis_connection.incrby(data.title, data.freq)
     end
   end
 
-  def remove_frequences_from_redis(redis_connection, file_input)
+  def remove_frequences_from_redis(redis_connection, file_input, opts)
     file_input.each_with_index do |line, i|
-      line = line.strip
-      fields = line.split(" ")
-      title = fields[1].split("_")[0..-2].join("_")
-      count = fields[0].to_i
+      data = line_to_data(line, opts)
 
-      value = redis_connection.decrby(title, count)
-      redis_connection.del(title) if value.to_i < 0
+      curr_freq = redis_connection.decrby(data.title, data.freq)
+      redis_connection.del(data.title) if curr_freq.to_i < 0
+    end
+  end
+
+  def line_to_data(line, opts={})
+    line_data = Struct.new :title, :freq
+    data = line_data.new
+
+    fields = line.strip.split(" ")
+    data.title = parse_location(fields[1], opts)
+    data.freq = fields[0].to_i
+
+    data
+  end
+
+  def parse_location(location, opts)
+    orientation = opts.fetch(:orientation, :forward)
+    if orientation == :forward
+      location.split("_")[0..-2].join("_")
+    elsif orientation == :reverse
+      location.split("_")[0] + "_" + location.split("_")[2]
+    else
+      "error"
     end
   end
 end
